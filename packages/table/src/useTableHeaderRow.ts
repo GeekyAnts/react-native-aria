f5ff6b9ecd5d1c20ba46fa0eba3c4dd2c5bddcd5
ankit-tailor <tailorankit3345@gmail.com>
2022-03-01 16:01:40 +0530

    feat: useTableHeaderRow

--- packages/table/src/useTableHeaderRow.ts
+++ packages/table/src/useTableHeaderRow.ts
@@ -1,6 +1,6 @@
 import { GridRowProps } from '@react-aria/grid';
 
-export function useTableHeaderRow<T>(props: GridRowProps<T>): any {
+export function useTableHeaderRow<T>(_props: GridRowProps<T>): any {
   return {
     rowProps: {},
   };
