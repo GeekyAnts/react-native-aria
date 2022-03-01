6e92224d726ce4f597d6720825a468c130516171
ankit-tailor <tailorankit3345@gmail.com>
2022-03-01 15:23:57 +0530

    feat:  export useTableRowGroup

--- packages/table/src/index.web.ts
+++ packages/table/src/index.web.ts
@@ -3,15 +3,4 @@ export * from './useTableCell.web';
 export * from './useTableHeaderRow.web';
 export * from './useTableRow.web';
 export * from './useTableColumnHeader.web';
-
-// Workaround for a Parcel bug where re-exports don't work in the CommonJS output format...
-// export {useGridRowGroup as useTableRowGroup} from '@react-aria/grid';
-import { mapDomPropsToRN } from '@react-native-aria/utils';
-
-export function useTableRowGroup(): any {
-  return {
-    rowGroupProps: mapDomPropsToRN({
-      role: 'rowgroup',
-    }),
-  };
-}
+export * from './useTableRowGroup.web';
