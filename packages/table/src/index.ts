568a654c760e989ec498391b1bdbf28279ada263
ankit-tailor <tailorankit3345@gmail.com>
2022-03-01 15:23:38 +0530

    feat: export useTableRowGroup

--- packages/table/src/index.ts
+++ packages/table/src/index.ts
@@ -3,12 +3,4 @@ export * from './useTableCell';
 export * from './useTableHeaderRow';
 export * from './useTableRow';
 export * from './useTableColumnHeader';
-
-// Workaround for a Parcel bug where re-exports don't work in the CommonJS output format...
-// export {useGridRowGroup as useTableRowGroup} from '@react-aria/grid';
-// import { GridRowGroupAria, useGridRowGroup } from '@react-aria/grid';
-export function useTableRowGroup(): any {
-  return {
-    rowGroupProps: {},
-  };
-}
+export * from './useTableRowGroup';
