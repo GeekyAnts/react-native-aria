bf8f427c5b6664b20932b843b2c988d47a039dea
ankit-tailor <tailorankit3345@gmail.com>
2022-03-01 15:46:12 +0530

    feat: useTableCell  for RN

--- packages/table/src/useTableCell.ts
+++ packages/table/src/useTableCell.ts
@@ -1,7 +1,24 @@
+import { GridNode } from '@react-types/grid';
+import { RefObject } from 'react';
+import { TableState } from '@react-stately/table';
+
+interface TableCellProps {
+  /** An object representing the table cell. Contains all the relevant information that makes up the row header. */
+  node: GridNode<unknown>;
+  /** Whether the cell is contained in a virtual scroller. */
+  isVirtualized?: boolean;
+  /**
+   * Handler that is called when a user performs an action on the cell.
+   * Please use onCellAction at the collection level instead.
+   * @deprecated
+   **/
+  onAction?: () => void;
+}
 //@ts-ignore
 export function useTableCell<T>(
-  //@ts-ignore
-  props: TableCellProps
+  _props: TableCellProps,
+  _state: TableState<T>,
+  _ref: RefObject<HTMLElement>
 ): any {
   return {
     gridCellProps: {},
