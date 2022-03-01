13bc00eb7a29f8d4a12109fb47df58887aab3d38
ankit-tailor <tailorankit3345@gmail.com>
2022-03-01 15:22:24 +0530

    fix: example import

--- example/storybook/stories/Table/Table.stories.tsx
+++ example/storybook/stories/Table/Table.stories.tsx
@@ -1,22 +1,12 @@
 import React from "react";
 import { storiesOf } from "@storybook/react-native";
-import TableExample from "./index";
-import NewTable from "./NewTable";
+import Table from "./Table";
 import { Wrapper } from "../Wrapper";
-import {
-  Cell,
-  Column,
-  Row,
-  TableBody,
-  TableHeader,
-  useTableState,
-} from "@react-stately/table";
-import { Text, View } from "react-native";
 
 export const Example = () => {
   return (
     <Wrapper>
-      <NewTable aria-label="Example static collection table" />
+      <Table aria-label="Example static collection table" />
     </Wrapper>
   );
 };
